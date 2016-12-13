d71b47abd6285b8dcbb5d72714edf8d16a96bf06
Chase Caynoski <cvc6644@rit.edu>
2016-12-13 14:54:38 -0500

    no message

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,28 +15,60 @@ angular.module('starter.controllers', [])
   $scope.updateClass = function (e){
       
       var spec =document.getElementById('spec');
+      
       spec.innerHTML="";
       for(specss in $scope.wow[e].specs){
           var speccc = $scope.wow[e].specs[specss];
           var out = document.createElement('option');
+          out.setAttribute('value',specss);
           out.innerHTML = speccc.name;
           spec.appendChild(out);
       }
       spec.removeAttribute('disabled');
-      
+      $scope.updateSpecs(0);
+      spec.onchange = function (e){
+          $scope.updateSpecs(e.target.selectedOptions[0].value);
+      }
   };
   $scope.updateSpecs = function (e){
-      
+      var list = document.getElementById('list');
+      list.removeAttribute('hidden');
+      var selectClass = document.getElementsByTagName('select')[0].selectedOptions[0].value;
+      var talents = $scope.wow[selectClass].specs[e].ranks;
+      var ss = document.getElementsByClassName('talentss');
+      for(talent in talents){
+          //1,3,5
+          var talentss =[ss[talent].childNodes[1],ss[talent].childNodes[3],ss[talent].childNodes[5]];
+          //talentss[0].childNodes[1].childNodes[0].childNodes[0].innerText = talents[talent].right.name;
+          
+          talentss[0].childNodes[1].childNodes[0].childNodes[0].innerText = "";
+          var img = document.createElement('img');
+          img.setAttribute('src',talents[talent].right.icon);
+          
+          talentss[0].childNodes[1].childNodes[0].appendChild(img);
+          //talentss[0].childNodes[1].childNodes[0].childNodes[0].appendChild(img);
+          talentss[1].childNodes[1].childNodes[0].childNodes[0].innerText = talents[talent].center.name;
+          
+          talentss[1].childNodes[1].childNodes[0].childNodes[0].innerText = "";
+          img = document.createElement('img');
+          img.setAttribute('src',talents[talent].center.icon);
+          talentss[1].childNodes[1].childNodes[0].appendChild(img);
+          talentss[2].childNodes[1].childNodes[0].childNodes[0].innerText = "";
+          img = document.createElement('img');
+          img.setAttribute('src',talents[talent].left.icon);
+          talentss[2].childNodes[1].childNodes[0].appendChild(img);
+          console.log(talents[talent].right.name);
+      }
   };
 })
 
 
 
 .controller('GW2Ctrl', function($scope,gw2) {
-  
+    $scope.gw2 = gw2.all();
 })
 .controller('DestinyCtrl', function($scope,destiny){
-    
+    $scope.destiny = destiny.all();
 })
 .controller('SwtorCtrl',function($scope,swtor){
     
